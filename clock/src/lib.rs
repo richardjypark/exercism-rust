9b0242e047148e2a610aef038f0d00a5773d52bb
richardjypark <richard@richardjypark.com>
2022-03-16 19:06:28 -0400

    change to minutes only

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,61 +1,33 @@
 use std::fmt;
+const MINUTES_PER_HOUR: i32 = 60;
+const MINUTES_PER_DAY: i32 = 60 * 24;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Clock {
-    hours: i32,
     minutes: i32,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let minute_hours = match minutes {
-            m if m >= 0 => m / 60,
-            m if m % 60 == 0 => m / 60,
-            m => m / 60 - 1,
-        };
-
-        let hours_converted = match (hours + minute_hours) % 24 {
-            h if h >= 0 => h,
-            h => 24 + h,
-        };
-
-        let minutes_converted = match (minutes) % 60 {
-            m if m >= 0 => m,
-            m => 60 + m,
-        };
-
         Clock {
-            hours: hours_converted,
-            minutes: minutes_converted,
+            minutes: (minutes + (hours * MINUTES_PER_HOUR)).rem_euclid(MINUTES_PER_DAY),
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let minute_hours = match minutes + self.minutes {
-            m if m >= 0 => m / 60,
-            m if m % 60 == 0 => m / 60,
-            m => m / 60 - 1,
-        };
-
-        let hours_converted = match (self.hours + minute_hours) % 24 {
-            h if h >= 0 => h,
-            h => 24 + h,
-        };
-
-        let minutes_converted = match (minutes + self.minutes) % 60 {
-            m if m >= 0 => m,
-            m => 60 + m,
-        };
-
         Self {
-            minutes: minutes_converted,
-            hours: hours_converted,
+            minutes: (self.minutes + minutes).rem_euclid(MINUTES_PER_DAY),
         }
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+        write!(
+            f,
+            "{:02}:{:02}",
+            self.minutes / MINUTES_PER_HOUR,
+            self.minutes % MINUTES_PER_HOUR
+        )
     }
 }
